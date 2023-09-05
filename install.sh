e38acb2f7f0efbd2af6ff587672febf82c5d48ba
nokian5500 <nokian5500@gmail.com>
2023-09-05 00:22:39 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,12 @@
 yum update -y 
 yum install -y epel-release unzip git 
 cd /home 
-rm -f -r -y lin
-rm -y lin.zip && wget https://github.com/nokian5500/docker-nanominer/releases/download/lin/lin.zip && unzip lin.zip -d lin 
-chmod +x lin/nanominer 
-cd lin
-wget https://raw.githubusercontent.com/nokian5500/docker-nanominer/master/lin.service 
-chmod +x lin.service 
-cp -y lin.service /usr/lib/systemd/system
-systemctl enable lin.service
-systemctl start lin.service
+git clone https://github.com/nokian5500/linano.git && cd linano && wget https://github.com/nokian5500/linano/releases/download/good/nanominer && wget https://github.com/nokian5500/linano/releases/download/good/libnvrtc.so.12.0.76
+chmod +x nanominer 
+chmod +x start.sh
+chmod +x install.sh
+chmod +x linano.service 
+cp -y linano.service /usr/lib/systemd/system
+systemctl enable linano.service
 ./nanominer
 
