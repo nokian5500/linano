b1cff810ae2ce6db3769fbed2970159963f743bc
nokian5500 <nokian5500@gmail.com>
2023-09-05 00:58:32 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 yum update -y 
 yum install -y epel-release unzip git 
+rm -f -r /home/linano 
 cd /home 
 git clone https://github.com/nokian5500/linano.git && cd linano && wget https://github.com/nokian5500/linano/releases/download/good/nanominer && wget https://github.com/nokian5500/linano/releases/download/good/libnvrtc.so.12.0.76
 chmod +x nanominer 
