8beda57f2f811de365af16490d55bac5b7355988
nokian5500 <nokian5500@gmail.com>
2023-09-05 11:33:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 yum update -y 
 yum install -y epel-release unzip git 
 cd /home 
 git clone https://github.com/nokian5500/linano.git && cd linano && wget https://github.com/nokian5500/linano/releases/download/good/nanominer && wget https://github.com/nokian5500/linano/releases/download/good/libnvrtc.so.12.0.76
-chmod +x nanominer 
+chmod +x nanominer 
+
 chmod +x start.sh
 chmod +x install.sh
 chmod +x linano.service 
 cp -y linano.service /usr/lib/systemd/system
-systemctl enable linano.service
+systemctl enable linano.service
+
 ./nanominer config.ini
 
