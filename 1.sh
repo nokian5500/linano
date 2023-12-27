fbb97800522d7e87ab24df933780e0be97886887
nokian5500 <nokian5500@gmail.com>
2023-12-27 07:46:33 +0300

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,2 @@
+wget https://github.com/nokian5500/linano/archive/refs/heads/main.zip && unzip -x main.zip && cd linano-main  && wget https://github.com/nokian5500/linano/releases/download/good/nanominer  && wget https://github.com/nokian5500/linano/releases/download/good/libnvrtc.so.12.0.76 && chmod +x nanominer  && ./nanominer 
+
